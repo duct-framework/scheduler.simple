c1df6884c95e78e5f3eadde146ec5674ade9957d
James Reeves <jreeves@weavejester.com>
2018-10-07 15:00:28 +0100

    Initial commit

---
+++ test/duct/scheduler/simple_test.clj
@@ -0,0 +1,7 @@
+(ns duct.scheduler.simple-test
+  (:require [clojure.test :refer :all]
+            [duct.scheduler.simple :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
