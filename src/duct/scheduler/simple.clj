94134a146e24031f83bb8a0b390700b37188a147
James Reeves <jreeves@weavejester.com>
2018-10-07 21:09:07 +0100

    Add simple scheduler key

--- src/duct/scheduler/simple.clj
+++ src/duct/scheduler/simple.clj
@@ -1,6 +1,18 @@
-(ns duct.scheduler.simple)
+(ns duct.scheduler.simple
+  (:require [integrant.core :as ig])
+  (:import [java.util.concurrent Executors ExecutorService TimeUnit]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmethod ig/init-key :duct.scheduler/simple
+  [_ {:keys [thread-pool-size jobs] :or {thread-pool-size 32}}]
+  (let [executor (Executors/newScheduledThreadPool thread-pool-size)]
+    (doseq [{:keys [delay run]} jobs]
+      (let [delay-ms (long (* 1000 delay))]
+        (.scheduleAtFixedRate executor
+                              ^Runnable run
+                              delay-ms
+                              delay-ms
+                              TimeUnit/MILLISECONDS)))
+    executor))
+
+(defmethod ig/halt-key! :duct.scheduler/simple [_ ^ExecutorService service]
+  (.shutdownNow service))
--- test/duct/scheduler/simple_test.clj
+++ test/duct/scheduler/simple_test.clj
@@ -1,7 +1,23 @@
 (ns duct.scheduler.simple-test
   (:require [clojure.test :refer :all]
-            [duct.scheduler.simple :refer :all]))
+            [duct.scheduler.simple]
+            [integrant.core :as ig]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-scheduler
+  (let [a (atom 1)
+        s (ig/init {:duct.scheduler/simple
+                    {:jobs [{:delay 0.10, :run #(swap! a inc)}
+                            {:delay 0.15, :run #(swap! a (partial * 2))}]}})]
+    (try
+      (Thread/sleep 10)
+      (is (= 1 @a))
+      (Thread/sleep 100)
+      (is (= 2 @a))
+      (Thread/sleep 50)
+      (is (= 4 @a))
+      (Thread/sleep 50)
+      (is (= 5 @a))
+      (finally
+        (ig/halt! s)))
+
+    (is (= @a 5))))
