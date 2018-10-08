9005b216a77823ce78d2059a0c4c98ab6fca7198
James Reeves <jreeves@weavejester.com>
2018-10-08 18:00:55 +0100

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject duct/scheduler.simple "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple scheduler for Integrant"
+  :url "https://github.com/duct-framework/scheduler.simple"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
