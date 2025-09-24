4ca5008696e2f3ae0dc53361c9ec793f6fac9703
James Reeves <jreeves@weavejester.com>
2025-09-24 15:22:08 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/duct-framework/scheduler.simple"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.1"]
-                 [integrant "0.13.1"]])
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [integrant "1.0.0"]])
